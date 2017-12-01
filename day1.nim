9c712909d277b92e5b05bbc8958c0ea2db7c5d58
Dominik Picheta <dominikpicheta@gmail.com>
2017-12-01 16:00:38 +0000

    Implement day 1 part two.

--- day1.nim
+++ day1.nim
@@ -11,19 +11,31 @@ proc `[]`(input: CircularString, i: int): char =
   let len = input.len
   return input.string[i - (len * (i div len))]
 
-proc solve(input: CircularString): int =
-  result = 0
+proc solve(input: CircularString): tuple[part1, part2: int] =
+  result = (0, 0)
   for i in 0..<input.len:
     if input[i] == input[i+1]:
-      result += parseInt($input[i])
+      result[0] += parseInt($input[i])
+    if input[i] == input[i+(input.len div 2)]:
+      result[1] += parseInt($input[i])
 
 when isMainModule:
-  doAssert solve("1122".CircularString) == 3
-  doAssert solve("1111".CircularString) == 4
-  doAssert solve("1234".CircularString) == 0
-  doAssert solve("91212129".CircularString) == 9
+  # Part 1
+  doAssert solve("1122".CircularString)[0] == 3
+  doAssert solve("1111".CircularString)[0] == 4
+  doAssert solve("1234".CircularString)[0] == 0
+  doAssert solve("91212129".CircularString)[0] == 9
+
+  # Part 2
+  doAssert solve("1212".CircularString)[1] == 6
+  doAssert solve("1221".CircularString)[1] == 0
+  doAssert solve("123425".CircularString)[1] == 4
+  doAssert solve("123123".CircularString)[1] == 12
+  doAssert solve("12131415".CircularString)[1] == 4
 
   let input = readFile("day1.input").strip().CircularString
-  let answer = input.solve()
-  echo("Answer: ", answer)
+  let (part1, part2) = input.solve()
+  echo("Part 1 answer: ", part1)
+  echo("Part 2 answer: ", part2)
+
 
